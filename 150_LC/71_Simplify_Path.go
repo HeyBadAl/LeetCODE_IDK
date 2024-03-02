416276cee05259a6a95ddfd638a5df68102b4014
Badal <badalWork1@gmail.com>
2024-03-02 21:57:48 +0530

    feat: solved simplify path
    - uses a simple stack to track of component
    - and according to that component do some tasks { idr }
    - Time:O(n)
    - Space: O(n) { worsts case, all are the symbols }

---
+++ 150_LC/71_Simplify_Path.go
@@ -0,0 +1,62 @@
+/*
+ Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
+
+In a Unix-style file system, a period '.' refers to the current directory, a double period '..' refers to the directory up a level, and any multiple consecutive slashes (i.e. '//') are treated as a single slash '/'. For this problem, any other format of periods such as '...' are treated as file/directory names.
+
+The canonical path should have the following format:
+
+The path starts with a single slash '/'.
+Any two directories are separated by a single slash '/'.
+The path does not end with a trailing '/'.
+The path only contains the directories on the path from the root directory to the target file or directory (i.e., no period '.' or double period '..')
+Return the simplified canonical path.
+*
+* Example 1:
+
+Input: path = "/home/"
+Output: "/home"
+Explanation: Note that there is no trailing slash after the last directory name.
+Example 2:
+
+Input: path = "/../"
+Output: "/"
+Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+Example 3:
+
+Input: path = "/home//foo/"
+Output: "/home/foo"
+Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	components := strings.Split(path, "/")
+	var stack []string
+
+	for _, component := range components {
+		switch component {
+		case "", ".":
+		case "..":
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		default:
+			stack = append(stack, component)
+		}
+	}
+
+	result := "/" + strings.Join(stack, "/")
+	return result
+}
+
+func main() {
+	result := simplifyPath("/home/badal/../aws/sd/../../idk/fuckkkkkkkkkkkkkkkkkkkkk")
+
+	fmt.Println(result)
+}
---
+++ 150_LC/go.mod
@@ -0,0 +1,3 @@
+module github.com/HeyBadAl/150
+
+go 1.22.0
